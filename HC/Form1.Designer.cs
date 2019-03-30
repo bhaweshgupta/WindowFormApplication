6aca7975b707ec6775192fe187a32e5aaaf8861f
Bhawesh Gupta <talktobhawesh@gmail.com>
2019-03-30 06:37:34 +0530

    first commit

--- HC/Form1.Designer.cs
+++ HC/Form1.Designer.cs
@@ -28,13 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbDatabase = new System.Windows.Forms.TextBox();
+            this.tbURL = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lbDatabase = new System.Windows.Forms.Label();
+            this.lbURL = new System.Windows.Forms.Label();
+            this.lbDBout = new System.Windows.Forms.Label();
+            this.lbURLOut = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbDatabase
+            // 
+            this.tbDatabase.Location = new System.Drawing.Point(83, 45);
+            this.tbDatabase.Name = "tbDatabase";
+            this.tbDatabase.Size = new System.Drawing.Size(267, 22);
+            this.tbDatabase.TabIndex = 0;
+            // 
+            // tbURL
+            // 
+            this.tbURL.Location = new System.Drawing.Point(83, 101);
+            this.tbURL.Name = "tbURL";
+            this.tbURL.Size = new System.Drawing.Size(267, 22);
+            this.tbURL.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(384, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "check DB";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(384, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "check URL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lbDatabase
+            // 
+            this.lbDatabase.AutoSize = true;
+            this.lbDatabase.Location = new System.Drawing.Point(12, 51);
+            this.lbDatabase.Name = "lbDatabase";
+            this.lbDatabase.Size = new System.Drawing.Size(69, 17);
+            this.lbDatabase.TabIndex = 4;
+            this.lbDatabase.Text = "Database";
+            // 
+            // lbURL
+            // 
+            this.lbURL.AutoSize = true;
+            this.lbURL.Location = new System.Drawing.Point(35, 106);
+            this.lbURL.Name = "lbURL";
+            this.lbURL.Size = new System.Drawing.Size(36, 17);
+            this.lbURL.TabIndex = 5;
+            this.lbURL.Text = "URL";
+            // 
+            // lbDBout
+            // 
+            this.lbDBout.AutoSize = true;
+            this.lbDBout.Location = new System.Drawing.Point(512, 45);
+            this.lbDBout.Name = "lbDBout";
+            this.lbDBout.Size = new System.Drawing.Size(46, 17);
+            this.lbDBout.TabIndex = 6;
+            this.lbDBout.Text = "label1";
+            // 
+            // lbURLOut
+            // 
+            this.lbURLOut.AutoSize = true;
+            this.lbURLOut.Location = new System.Drawing.Point(512, 106);
+            this.lbURLOut.Name = "lbURLOut";
+            this.lbURLOut.Size = new System.Drawing.Size(46, 17);
+            this.lbURLOut.TabIndex = 7;
+            this.lbURLOut.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(630, 228);
+            this.Controls.Add(this.lbURLOut);
+            this.Controls.Add(this.lbDBout);
+            this.Controls.Add(this.lbURL);
+            this.Controls.Add(this.lbDatabase);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbURL);
+            this.Controls.Add(this.tbDatabase);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbDatabase;
+        private System.Windows.Forms.TextBox tbURL;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lbDatabase;
+        private System.Windows.Forms.Label lbURL;
+        private System.Windows.Forms.Label lbDBout;
+        private System.Windows.Forms.Label lbURLOut;
     }
 }
 
--- HC/Form1.cs
+++ HC/Form1.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Net;
 
 namespace HC
 {
@@ -14,7 +11,78 @@ namespace HC
     {
         public Form1()
         {
+            
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tbDatabase != null && !string.IsNullOrWhiteSpace(tbDatabase.Text))
+            {
+                string s = dbcheck(tbDatabase.Text.ToString());
+                if (s.Length != 0)
+                    lbDBout.Text = s;
+                else
+                    lbDBout.Text = "Database don't exist";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (tbURL != null && !string.IsNullOrWhiteSpace(tbURL.Text))
+            {
+                bool idontcareatall = WebSiteIsAvailable(tbURL.Text);
+                lbURLOut.Text = idontcareatall.ToString();
+            }
+        }
+
+
+
+        public static string dbcheck(string str)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("spDBcheck", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@arg", str);
+
+                con.Open();
+                var obj = cmd.ExecuteScalar();
+                return obj.ToString();
+
+            }
+
+        }
+        private bool WebSiteIsAvailable(string Url)
+        {
+            string Message = string.Empty;
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(Url);
+
+            //var isSecured=request.Scheme;
+            //Console.WriteLine(isSecured);
+            //			if(isSecured)
+            //				Console.WriteLine(true.ToString());
+            //			else
+            //				Console.WriteLine(false.ToString());
+
+
+            request.Credentials = System.Net.CredentialCache.DefaultCredentials;
+            request.Method = "GET";
+
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+
+                }
+            }
+            catch (WebException ex)
+            {
+                Message += ((Message.Length > 0) ? "\n" : "") + ex.Message;
+            }
+
+            return (Message.Length == 0);
         }
     }
 }
